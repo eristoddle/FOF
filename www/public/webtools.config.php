21d77a3fe56e147dc45fe98ca89cdc43e234092d
Stephan Miller <stephanmil@gmail.com>
2014-01-02 00:36:01 -0600

    fix webtools link

--- www/public/webtools.config.php
+++ www/public/webtools.config.php
@@ -1,4 +1,5 @@
 <?php
-define('PTOOLSPATH', '/usr/local/bin/phalcon');
+//TODO: Hacked because dev tools not in path on vagrant box
+define('PTOOLSPATH', '/home/vagrant/phalcon-devtools');
 /* you can set ADMINIP as IP 192.168.0.1 or SUBNET 192. or 10.0.2. or 86.84.124. */
 define('ADMINIP', '192.168.');
