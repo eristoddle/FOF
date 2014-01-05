ec91fb2335cd1c89a13e25f1376bfb79993e7a81
Stephan Miller <stephanmil@gmail.com>
2014-01-05 15:06:41 -0600

    not sure yet how to handle gumby. activate the script to install it while provisioning the vm or making it part of the project. if installed when provisioning, then I should do that with the Phalcon project skeleton too, but there are bugs that currently require manual editing of files after generation, so stuck with this currently.

---
+++ vagrant/shell/install_gumby.sh
@@ -0,0 +1,8 @@
+mkdir /var/www/public/bower_components
+cd /var/www/public/bower_components
+git clone https://github.com/GumbyFramework/Gumby.git
+cd /var/www/public
+claymate install
+rm /var/www/public/index.html
+rm /var/www/public/ui.html
+rm -rf /var/www/public/bower_components/Gumby
--- vagrant/shell/install_nodejs.sh
+++ vagrant/shell/install_nodejs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 sudo su
-mkdir ~/local
-echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
-. ~/.bashrc
-cd ~/local
+cd
 git clone git://github.com/joyent/node.git
 cd node
-./configure --prefix=~/local
+./configure
 make install
-cd ~/local
+cd
+rm -rf node
 git clone git://github.com/isaacs/npm.git
 cd npm
 make install
+cd
+rm -rf npm
 npm install -g claymate
 npm install -g grunt
 npm install -g bower
